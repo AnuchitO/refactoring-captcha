823beff3e63ab43a27564d3ef3753a4b0fc9616f
anuchitop <anuchitp@thoughtworks.com>
2020-11-05 16:04:50 +0700

    4.3 ValueNode implement Node

--- src/main/java/com/thoughtworks/xconf/th/captcha/ValueNode.java
+++ src/main/java/com/thoughtworks/xconf/th/captcha/ValueNode.java
@@ -1,9 +1,19 @@
 package com.thoughtworks.xconf.th.captcha;
 
-public class ValueNode {
+public class ValueNode implements Node {
     protected Integer value;
 
     public ValueNode(Integer value) {
         this.value = value;
+    }
+
+    @Override
+    public Integer compute() {
+        return value;
+    }
+
+    @Override
+    public String display() {
+        return value.toString();
     }
 }
