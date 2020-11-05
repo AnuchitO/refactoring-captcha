cc033edb4031441645fc88f7d841728f842e74b2
anuchitop <anuchitp@thoughtworks.com>
2020-11-05 16:11:27 +0700

    4.5 remove extend ValueNode and not use contructor

--- src/main/java/com/thoughtworks/xconf/th/captcha/GodNode.java
+++ src/main/java/com/thoughtworks/xconf/th/captcha/GodNode.java
@@ -1,6 +1,6 @@
 package com.thoughtworks.xconf.th.captcha;
 
-public class GodNode extends ValueNode implements Node {
+public class GodNode implements Node {
 
     private String operator = "#";
 
@@ -8,14 +8,9 @@ public class GodNode extends ValueNode implements Node {
     private Node rightOperand;
 
     private GodNode(String operator, Node leftOperand, Node rightOperand) {
-        super(0);
         this.operator = operator;
         this.leftOperand = leftOperand;
         this.rightOperand = rightOperand;
-    }
-
-    private GodNode(Integer value) {
-        super(value);
     }
 
     public static Node valueNode(Integer value) {
