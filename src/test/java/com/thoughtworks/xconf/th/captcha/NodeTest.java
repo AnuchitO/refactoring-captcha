05b6f4b1cb5c973a9b3f33a8ce0227f14d1aadb9
anuchitop <anuchitp@thoughtworks.com>
2020-11-05 15:55:32 +0700

    3. extract interface. rename existing class to GodNode

--- src/test/java/com/thoughtworks/xconf/th/captcha/NodeTest.java
+++ src/test/java/com/thoughtworks/xconf/th/captcha/NodeTest.java
@@ -9,40 +9,40 @@ public class NodeTest {
 
     @Test
     public void display_ValueNode_printsTheValue() {
-        Node valueNode = Node.valueNode(1);
+        Node valueNode = GodNode.valueNode(1);
         assertEquals("1", valueNode.display());
     }
 
     @Test
     public void display_OperatorNode_printsTheEquation() {
-        Node operatorNode = Node.opNode("+", Node.valueNode(1), Node.valueNode(2));
+        Node operatorNode = GodNode.opNode("+", GodNode.valueNode(1), GodNode.valueNode(2));
         assertEquals("1 + 2", operatorNode.display());
     }
 
     @Test
     public void display_complexOperatorNodes() {
-        Node f = Node.opNode("*", Node.valueNode(2), Node.valueNode(5));
-        Node g = Node.opNode("+", f, Node.valueNode(3));
+        Node f = GodNode.opNode("*", GodNode.valueNode(2), GodNode.valueNode(5));
+        Node g = GodNode.opNode("+", f, GodNode.valueNode(3));
 
         assertEquals("2 * 5 + 3", g.display());
     }
 
     @Test
     public void compute_ValueNode_givesTheValueBack() {
-        Node valueNode = Node.valueNode(2);
+        Node valueNode = GodNode.valueNode(2);
         assertEquals(2, valueNode.compute().intValue());
     }
 
     @Test
     public void compute_OperatorNode_givesTheComputedResult() {
-        Node opsNode = Node.opNode("-", Node.valueNode(5), Node.valueNode(3));
+        Node opsNode = GodNode.opNode("-", GodNode.valueNode(5), GodNode.valueNode(3));
         assertEquals(2, opsNode.compute().intValue());
     }
 
     @Test
     public void compute_complexOperatorNodes() {
-        Node f = Node.opNode("*", Node.valueNode(2), Node.valueNode(5));
-        Node g = Node.opNode("+", f, Node.valueNode(3));
+        Node f = GodNode.opNode("*", GodNode.valueNode(2), GodNode.valueNode(5));
+        Node g = GodNode.opNode("+", f, GodNode.valueNode(3));
 
         assertEquals(13, g.compute().intValue());
     }
